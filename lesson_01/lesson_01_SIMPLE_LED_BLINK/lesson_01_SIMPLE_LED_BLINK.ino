985656832f23b41d03630c090771fbc3390bc14f
Breanna Higgins <bbhiggins520@outlook.com>
2017-10-16 08:45:08 -0400

    Whoops, fixed params on pinMode function

---
+++ lesson_01/lesson_01_SIMPLE_LED_BLINK/lesson_01_SIMPLE_LED_BLINK.ino
@@ -0,0 +1,32 @@
+/**
+ * This simple program explains how to make an LED blink based
+ * on energia code supplied to the MSP430G2553 launchpad.
+ * 
+ * This simple blinker flashes LED1
+ */
+
+ #define TIME_DELAY_MS 750;
+ 
+void setup() {
+  // Configure IO
+   pinMode(RED_LED, OUTPUT);
+
+   //set up your serial console
+   Serial.begin(9600); // 9600 bps Baud Rate
+   Serial.printf("Let's blink an LED!");
+
+}
+
+void loop() {
+  // Turn the LED ON
+  digitalWrite(RED_LED, HIGH);
+  
+  // Wait 
+  delay(TIME_DELAY_MS);
+
+  // Turn the LED OFF
+  digitalWrite(RED_LED, LOW);
+
+  // Wait
+  delay(TIME_DELAY_MS);
+}
